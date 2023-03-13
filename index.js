49c550c5bfdbf141f099a914de31ac1d6b192202
Bblissful <101343866+Bblissful@users.noreply.github.com>
2023-03-13 23:29:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+     let play = document.getElementById("play");
+     function playMusic(){
+    var music = new Audio("pray.mp3");
+     music.play();
+     }
+
+     play.addEventListener("click", playMusic);
+       
